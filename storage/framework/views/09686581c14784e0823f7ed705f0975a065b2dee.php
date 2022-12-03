aaa6cfbaa67e2fb79e6ebd8979aaceea1a8c30bd
niftywebsite <mail@getanifty.website>
2022-12-03 18:40:25 +0200

    remove sass from GHA

--- storage/framework/views/09686581c14784e0823f7ed705f0975a065b2dee.php
+++ storage/framework/views/09686581c14784e0823f7ed705f0975a065b2dee.php
@@ -12,7 +12,7 @@
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     
-    <?php echo app('Illuminate\Foundation\Vite')(["resources/css/app.css","resources/scss/app.scss","resources/js/app.js",]); ?>
+    <?php echo app('Illuminate\Foundation\Vite')(["resources/scss/app.scss","resources/js/app.js",]); ?>
 </head>
 <body>
 <?php echo $__env->make('partials/_header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
