1b6b474dd8e8be5451f144016c6a84a2a71f0ce1
niftywebsite <mail@getanifty.website>
2022-12-04 15:34:22 +0200

    add server to vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
+    server: {
+        host: true,
+        hmr: {
+            host: "localhost",
+        },
+    },
     plugins: [
         laravel({
-            input: [
-                "resources/scss/app.scss",
-                "resources/js/app.js",
-            ],
+            input: ["resources/scss/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
     ],
