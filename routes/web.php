de28eb15982b02704b1f4903ae64265f56536622
niftywebsite <mail@getanifty.website>
2022-12-25 11:46:00 +0200

    Added photos page

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth', 'verified')->group(function () {
     Route::get('/', [PageController::class, 'edit'])->name('main');
     Route::patch('/', [PageController::class, 'update'])->name('main.update');
-    Route::get('/contacts', function () {
-        return view('contacts');
-    })->name('contacts');
     Route::get('/photos', function () {
         return view('photos');
     })->name('photos');
+    Route::get('/contacts', function () {
+        return view('contacts');
+    })->name('contacts');
     Route::get('/education', function () {
         return view('education');
     })->name('education');
