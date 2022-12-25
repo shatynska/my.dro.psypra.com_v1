dde64e5318c1657af3e0bcc13def24d90f52494e
niftywebsite <mail@getanifty.website>
2022-12-25 12:28:38 +0200

    added base of about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Розклад') }}
+            {{ __('Про себе') }}
         </h2>
     </x-slot>
 
