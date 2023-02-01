9efa4b72d91c7b32a140d2b2b66b5253dc146700
niftywebsite <mail@getanifty.website>
2023-02-01 22:25:53 +0200

    fixed bag with AttributeSeeder

--- database/seeders/AttributeSeeder.php
+++ database/seeders/AttributeSeeder.php
@@ -16,74 +16,76 @@ class AttributeSeeder extends Seeder
     public function run()
     {
 
-        Attribute::create(
+        Attribute::insert(
             [
-                'title' => 'спеціальності',
-                'database' => 'specialties',
-                'is_extendable' => '1',
-                'is_main_attribute' => '1',
-            ],
-            [
-                'title' => 'фахівці',
-                'database' => 'specialists',
-                'is_extendable' => '0',
-                'is_main_attribute' => '0',
+                [
+                    'title' => 'спеціальності',
+                    'database' => 'specialties',
+                    'is_extendable' => '1',
+                    'is_main_attribute' => '1',
+                ],
+                [
+                    'title' => 'фахівці',
+                    'database' => 'specialists',
+                    'is_extendable' => '0',
+                    'is_main_attribute' => '0',
 
-            ],
-            [
-                'title' => 'форми роботи',
-                'database' => 'quantities',
-                'is_extendable' => '1',
-                'is_main_attribute' => '1',
-            ],
-            [
-                'title' => 'вікові групи',
-                'database' => 'ages',
-                'is_extendable' => '1',
-                'is_main_attribute' => '1',
-            ],
-            [
-                'title' => 'тривалість',
-                'database' => 'durations',
-                'is_extendable' => '1',
-                'is_main_attribute' => '1',
-            ],
-            [
-                'title' => 'напрямки терапії',
-                'database' => 'directions',
-                'is_extendable' => '1',
-                'is_main_attribute' => '1',
-            ],
-            [
-                'title' => 'вартість',
-                'database' => 'prices',
-                'is_extendable' => '0',
-                'is_main_attribute' => '0',
-            ],
-            [
-                'title' => 'запити',
-                'database' => 'queries',
-                'is_extendable' => '1',
-                'is_main_attribute' => '0',
-            ],
-            [
-                'title' => 'місця',
-                'database' => 'places',
-                'is_extendable' => '0',
-                'is_main_attribute' => '0',
-            ],
-            [
-                'title' => 'години',
-                'database' => 'hours',
-                'is_extendable' => '0',
-                'is_main_attribute' => '0',
-            ],
-            [
-                'title' => 'контакти',
-                'database' => 'contacts',
-                'is_extendable' => '0',
-                'is_main_attribute' => '0',
-            ],
+                ],
+                [
+                    'title' => 'форми роботи',
+                    'database' => 'quantities',
+                    'is_extendable' => '1',
+                    'is_main_attribute' => '1',
+                ],
+                [
+                    'title' => 'вікові групи',
+                    'database' => 'ages',
+                    'is_extendable' => '1',
+                    'is_main_attribute' => '1',
+                ],
+                [
+                    'title' => 'тривалість',
+                    'database' => 'durations',
+                    'is_extendable' => '1',
+                    'is_main_attribute' => '1',
+                ],
+                [
+                    'title' => 'напрямки терапії',
+                    'database' => 'directions',
+                    'is_extendable' => '1',
+                    'is_main_attribute' => '1',
+                ],
+                [
+                    'title' => 'вартість',
+                    'database' => 'prices',
+                    'is_extendable' => '0',
+                    'is_main_attribute' => '0',
+                ],
+                [
+                    'title' => 'запити',
+                    'database' => 'queries',
+                    'is_extendable' => '1',
+                    'is_main_attribute' => '0',
+                ],
+                [
+                    'title' => 'місця',
+                    'database' => 'places',
+                    'is_extendable' => '0',
+                    'is_main_attribute' => '0',
+                ],
+                [
+                    'title' => 'години',
+                    'database' => 'hours',
+                    'is_extendable' => '0',
+                    'is_main_attribute' => '0',
+                ],
+                [
+                    'title' => 'контакти',
+                    'database' => 'contacts',
+                    'is_extendable' => '0',
+                    'is_main_attribute' => '0',
+                ],
+            ]
         );
     }
 }
