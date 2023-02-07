4fed5f5356a2e997c0e470420cb1806cc75260bc
niftywebsite <mail@getanifty.website>
2023-02-07 17:38:23 +0200

    replaced logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('main.index') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <x-application-logo class="block h-12 invert opacity-70" />
                     </a>
                 </div>
 
