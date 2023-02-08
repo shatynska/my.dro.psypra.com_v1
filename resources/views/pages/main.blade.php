d643495ea509481617d7ca017e95b9b8ab3b26fa
niftywebsite <mail@getanifty.website>
2023-02-08 13:59:50 +0200

    test GHA-6

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -14,7 +14,7 @@
                     @csrf
                     @method('patch')
 
-                    <div>
+                    <div class="pt-20">
                         <x-input-label for="name" :value="__('Name')" />
                         <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $specialist->name)" required autocomplete="name" />
                         <x-input-error class="mt-2" :messages="$errors->get('name')" />
