b1bfccf64dce7e325b47903c14af35d2f34c93de
niftywebsite <mail@getanifty.website>
2023-02-20 23:43:15 +0200

    Add update form for method page

--- resources/views/pages/method.blade.php
+++ resources/views/pages/method.blade.php
@@ -1,17 +1,46 @@
 <x-app-layout>
+    
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Мій підхід') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("Сторінка в процесі розробки") }}
-                </div>
-            </div>
+    <x-u-section>
+        <div class="max-w-xl">
+            <section>
+
+                <form method="post" action="{{ route('method.update') }}" class="space-y-6">
+                    @csrf
+                    @method('patch')
+
+                    <div>
+                        <x-input-label for="text" :value="__('Мій підхід')" />
+                        <textarea name="text" id="text"  rows="16" class="mt-1 block w-full text-gray-700 border-gray-300">{{ $specialist->method }}</textarea>
+                        <x-input-error class="mt-2" :messages="$errors->get('method')" />
+                    </div>
+ 
+                    
+                    
+                    <div class="flex items-center gap-4">
+                        <x-primary-button>{{ __('Save') }}</x-primary-button>
+
+                        @if (session('status') === 'main-updated')
+                            <p
+                                x-data="{ show: true }"
+                                x-show="show"
+                                x-transition
+                                x-init="setTimeout(() => show = false, 2000)"
+                                class="text-sm text-gray-600"
+                            >{{ __('Saved') }}</p>
+                        @endif
+                    </div>
+                    
+                </form>
+            </section>
         </div>
-    </div>
+    </x-u-section>
+
 </x-app-layout>
+
+
