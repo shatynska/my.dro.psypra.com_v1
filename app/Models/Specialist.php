21855d91f668807a3975d149f7d687d5b572f61e
shatynska <shatynskaa@gmail.com>
2023-03-05 13:37:08 +0200

    Install Spatie media library

--- app/Models/Specialist.php
+++ app/Models/Specialist.php
@@ -21,11 +21,11 @@ use App\Models\Contacts\OnlineContact;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Spatie\MediaLibrary\InteractsWithMedia;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 
 class Specialist extends Model implements HasMedia
--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,15 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
+        'media' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/media'),
+            'url'    => env('APP_URL') . '/storage/media',
             'visibility' => 'public',
             'throw' => false,
         ],
